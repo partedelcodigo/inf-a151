e5490ef9e42672f2b6367e533b66a09703745a1f
Gaston Nina <gastonnina@gmail.com>
2013-03-30 19:50:33 -0400

    adicioandno categorias listado

--- assets/www/js/main-functions.js
+++ assets/www/js/main-functions.js
@@ -31,6 +31,7 @@ function IsEmail(email) {
 
 var db;
 var data2Save = new Array();
+var data2SaveCat = new Array;
 document.addEventListener( "deviceready", function() {
 	document.addEventListener( "online", function() {
         //--$.mobile.changePage("#page_contact", "slideup");
@@ -42,7 +43,54 @@ document.addEventListener( "deviceready", function() {
 	
     checkUpdates();
 } );
-            
+checkUpdates();//gaston
+//filterBrand('Laptops');//gaston
+var bus={
+    category:'Laptops',
+    brand:'Apple',
+    filterBrand:function(category){
+        var sql_c="SELECT brand FROM products WHERE category='"+category+"' GROUP BY brand"
+        //console.log("--->" + sql_c);
+        db.transaction(function(tx) {
+            tx.executeSql(sql_c, [],
+                    function(tx, results) {
+                        var len = results.rows.length, i,j,dBhtml,c,image;
+                        if (len > 1) {
+                            //console.log("--->crear listado");
+                            dBhtml='';
+                            c=1;
+                            for (i = 0; i < len; i++) {
+                               //console.log("--brand->" + results.rows.item(i).brand);
+                                for (j = 0; j < data2SaveCat.length; j++) {
+                                    if(data2SaveCat[j]['title']==results.rows.item(i).brand)
+                                        image=data2SaveCat[j]['image'];
+                                }
+                                if (c == 1) {
+                                    type = 'a';
+                                } else {
+                                    if (c == 2) {
+                                        type = 'b';
+                                    } else {
+                                        type = 'c';
+                                    }
+                                }
+                                dBhtml+='<div class="ui-block-'+type+'"><a href="#" onclick="bus.brand=\''+results.rows.item(i).brand+'\'"><img src="images/'+image+'"/></a></div>';
+                                c++;
+                                if(c==4){
+                                    c=1;
+                                }
+                            }
+                        } else {
+                            console.log("--->limpiar");
+                            dBhtml='';
+                        }
+                        $('#div_brand').html(dBhtml);
+                    }
+            );
+        });
+        
+    }
+};
 function checkUpdates() {
     $.ajax({
 		url:'http://www.codigobase.com/all-brands/getData.php',
@@ -88,9 +136,9 @@ function querySuccess( tx, result ) {
 	var len = result.rows.length;
 	
 	for( var i = 0; i < len; i++ ) {
-		console.log( "Names: " + result.rows.item(i).title );
-		console.log( "Names: " + result.rows.item(i).price );
-		console.log( "Names: " + result.rows.item(i).status );
+		//console.log( "Names: " + result.rows.item(i).title );
+		//console.log( "Names: " + result.rows.item(i).price );
+		//console.log( "Names: " + result.rows.item(i).status );
 	}
 }
 
@@ -106,10 +154,10 @@ function populateDB( tx ) {
 	for( var i = 0; i < data2Save.length; i++ ) {
 		desc = data2Save[i].description;
 		desc = addslashes( desc );
-		console.log("desc: " + desc);
+		//console.log("desc: " + desc);
 		desc = '';
 		query = "INSERT INTO products(title, description, price, brand, category, status) VALUES('" + data2Save[i].title + "', '" + desc + "','" + data2Save[i].price + "','" + data2Save[i].brand + "','" + data2Save[i].category + "','" + data2Save[i].status + "')";
-		console.log( query );
+		//console.log( query );
 		tx.executeSql( query );
 		//console.log( "INSERT INTO products(title, description, price, brand, category, status) VALUES('" + data2Save[i].title + "', '" + ; + "','" + data2Save[i].price + "','" + data2Save[i].brand + "','" + data2Save[i].category + "','" + data2Save[i].status + "')" );
 	}
@@ -124,7 +172,7 @@ function populateDB( tx ) {
         var createTable = "CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL,description BLOB NULL,image TEXT NOT NULL)";
         tx.executeSql( createTable );
         
-        var data2SaveCat = new Array;
+        data2SaveCat = new Array;
         data2SaveCat.push({title:"Apple",description:"",image:"logo_apple.png"});
         data2SaveCat.push({title:"Asus",description:"",image:"logo_asus.png"});
         data2SaveCat.push({title:"Hp",description:"",image:"logo_hp.png"});
@@ -193,6 +241,7 @@ function enviaContacto() {
             alert("Mensaje No Enviado, intente mas tarde");
         }
 
+                                    
     });
     return false;
 }
